d83be4a53f691fdba8fff2bcf9807d827b0f785c
1286259340@qq.com <Gitee>
2025-12-04 16:50:57 +0800

    初稿项目

---
+++ src/main/java/com/app/springbootcargovision/SpringbootCargoVisionApplication.java
@@ -0,0 +1,13 @@
+package com.app.springbootcargovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCargoVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCargoVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/springbootcargovision/SpringbootCargoVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootcargovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCargoVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
